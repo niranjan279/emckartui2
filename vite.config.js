42af5f726c2d8307a5d0da428feb08e1101794ca
niranjan279 <rajmohan.niranjan2@gmail.com>
2025-04-07 16:19:24 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-  }
+  base: '/', // adjust if needed for subdirectory deploys
 })
